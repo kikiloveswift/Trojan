c6330c38612371003f581743edc817eeb454c843
liqiangbj01 <liqiangbj01@yuanfudao.com>
2021-09-24 02:15:40 +0800

    auto push

--- crontabWeekly.sh
+++ crontabWeekly.sh
@@ -2,8 +2,8 @@
 
 script_path = "/var/autoScript/"
 # 这里的-x 参数判断$script_path是否存在并且是否具有可执行权限
-if [ ! -d "$script_path" ];then
-mkdir "$script_path"
+if [ ! -d "/var/autoScript/" ];then
+mkdir /var/autoScript/
 fi
 
 rm -rf /var/autoScript/*
